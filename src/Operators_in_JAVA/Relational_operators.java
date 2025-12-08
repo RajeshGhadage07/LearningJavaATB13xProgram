16e91b80995ca7aa51b88bfee5a027426f917178
Rajesh <rajesh.ghadage007@gmail.com>
2025-12-08 23:05:30 +0530

    Relational Operator file

---
+++ src/Operators_in_JAVA/Relational_operators.java
@@ -0,0 +1,23 @@
+package Operators_in_JAVA;
+
+public class Relational_operators {
+    static void main(String[] args) {
+//        <   ---- Less than
+//        >   ---- Greater than
+//        < = ---- Less than equal to
+//        > = ---- Greater than equal to
+//        = = ---- Equal to
+//        ! = ---- Not equal to
+
+       int a = 103;
+       int b = 107;
+       boolean c = a>b;
+        System.out.println(c);
+
+        int age_Rajesh = 33;
+        int age_anu = 30;
+        boolean result = age_Rajesh>=age_Rajesh;
+        System.out.println(result);
+
+    }
+}
